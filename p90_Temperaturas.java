5399b155261e13a44dd2e6f5e0d9720ecdfee8e2
Amaury223 <jaimeamauryhz@gmail.com>
2024-09-25 23:51:54 -0600

    Práctica 7

--- p90_Temperaturas.java
+++ p90_Temperaturas.java
@@ -23,6 +23,7 @@ public class p90_Temperaturas {
 
             for (int i = 0; i<=8; i++)
                 System.out.print(Temperaturas[i] + "  ");
-       
+           
+                
     }
 }
--- p91_MayoresPromedio.java
+++ p91_MayoresPromedio.java
@@ -1,31 +1,66 @@
+
 import java.util.Scanner;
+
 public class p91_MayoresPromedio {
 
-    public static int suma(int n,int[] x){
-        int suma=0;
+    public static void Mostrar(float[] x) {
+        System.out.println();
+        System.out.print("Calificaciones: ");
+        for (int i = 0; i < x.length; i++) {
+            System.out.print(x[i] + " ");
+        }
+    }
 
-        for (int i = 0 ; i<=n ; i++){
-            suma = suma + x[i]; 
+    public static void guardarCalificaciones(Scanner obj, float[] cal, int n) {
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print("Calificación " + (i + 1) + ": ");
+            cal[i] = obj.nextFloat();
+        }
+    }
+
+    public static float Suma(float[] cals) {
+        float suma = 0;
+        for (int i = 0; i < cals.length; i++) {
+            suma += cals[i];   
         }
         return suma;
     }
-    public static void main(String[] args) {
-    
-        Scanner obj = new Scanner(System.in);
 
-        System.out.print("\033[H\033[2J");System.out.flush();
-        
-        int n,x;
-        
-        System.out.print("Numero de calificaciones? ");n=obj.nextInt();
-        int [] calificaciones = new int[n];
+    public static float Promedio(float suma, int n){
+        return suma / n;
+    }
 
-        for(int i = 0; i<n ; i++){
-            System.out.println("Calificacion " + i + " ? ");x = obj.nextInt();
-            calificaciones[i]=x; 
+    public static int CalMayoresPromedio(float[] cals, float promedio) {
+        int totalMayores = 0;
+        System.out.println("\nCalificaciones mayores al promedio:");
+        for (int i = 0; i < cals.length; i++) {
+            if (cals[i] > promedio) {
+                System.out.print(cals[i] + " ");
+                totalMayores++;
+            }
         }
+        System.out.println();
+        return totalMayores;
+    }
 
-        System.out.println(calificaciones);
-        suma(n, calificaciones);
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");System.out.flush();
+        Scanner obj = new Scanner(System.in);
+        
+        System.out.print("Cuantas calificaciones vas a ingresar?: ");
+        int n = obj.nextInt();
+        float [] cal = new float[n];
+
+        guardarCalificaciones(obj, cal, n);
+        Mostrar(cal);
+
+        float suma = Suma(cal);
+        float promedio = Promedio(suma, n);
+        System.out.println("\nSuma: " + suma);
+        System.out.println("\nPromedio: " + promedio);
+
+        int totalMayores = CalMayoresPromedio(cal, promedio);
+        System.out.println("\nCantidad de calificaciones mayores al promedio: " + totalMayores);
     }
 }
