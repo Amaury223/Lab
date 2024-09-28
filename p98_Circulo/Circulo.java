78cfcfbab3f26dff6fb5631d6226b55575243697
Amaury223 <jaimeamauryhz@gmail.com>
2024-09-28 13:46:15 -0600

    Practica 8

---
+++ p98_Circulo/Circulo.java
@@ -0,0 +1,36 @@
+package p98_Circulo;
+
+public class Circulo {
+    private double Radio;
+    private double Area;
+    private double Circunferencia;
+
+    
+    public Circulo() {
+    }
+    public Circulo(double radio) {
+        Radio = radio;
+    }
+
+
+    public double getRadio() {
+        return Radio;
+    }
+    public void setRadio(double radio) {
+        Radio = radio;
+    }
+
+    public double getArea() {
+        Area = Math.PI * Math.pow(Radio, 2);
+        return Area;
+    }
+    public double getCircunferencia() {
+        Circunferencia = 2 * Radio * Math.PI;
+        return Circunferencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo [Radio=" + Radio + "]";
+    }
+}
