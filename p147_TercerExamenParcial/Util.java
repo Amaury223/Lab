d25642087c479cb76469e0d075f1923c96b41d83
Amaury223 <jaimeamauryhz@gmail.com>
2024-11-14 09:12:42 -0600

    Actividad 26

--- p147_TercerExamenParcial/Util.java
+++ p147_TercerExamenParcial/Util.java
@@ -8,24 +8,21 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Util {
-    public static ArrayList<Jugador> inicializarDatos() {
-        ArrayList<Jugador> datos = new ArrayList<>();
-        datos.add(new Jugador("Juan Perez", 22, 'M', "Soltero", "Delantero", 1000.0));
-        datos.add(new Jugador("Maria Jimenez", 18, 'F', "Soltera", "Portera", 1200.0));
-        datos.add(new Jugador("Carlos Fuentes", 30, 'M', "Casado", "Defensor", 1100.0));
+    public static ArrayList <Jugador> inicializarDatos() {
+        ArrayList <Jugador> datos = new ArrayList<>();
         return datos;
     }
 
-    public static void grabarDatos(String archivo, ArrayList<Jugador> datos) throws IOException {
+    public static void grabarDatos(String archivo, ArrayList <Jugador> datos) throws IOException {
         ObjectOutputStream fdatos = new ObjectOutputStream(new FileOutputStream(archivo));
         fdatos.writeObject(datos);
         fdatos.close();
     }
 
-    public static ArrayList<Jugador> leerDatos(String archivo) throws IOException, ClassNotFoundException {
-        ArrayList<Jugador> datos;
-        ObjectInputStream fdatos = new ObjectInputStream(new FileInputStream(archivo));
-        datos = (ArrayList<Jugador>) fdatos.readObject();
+    public static ArrayList <Jugador> leerDatos (String archivo) throws IOException, ClassNotFoundException {
+        ArrayList<Jugador> datos = new ArrayList<>();
+        ObjectInputStream fdatos = new ObjectInputStream (new FileInputStream(archivo));
+        datos = (ArrayList <Jugador>) fdatos.readObject();
         fdatos.close();
         return datos;
     }
